9cdebbd0858c6b5476d014240707030450803971
Steen Larsen <steen666@gmail.com>
2011-12-25 01:22:06 +0100

    no more iframes

--- out/client.js
+++ out/client.js
@@ -2,22 +2,34 @@ goog.provide('client');
 goog.require('cljs.core');
 goog.require('goog.net.XhrIo');
 goog.require('goog.dom');
-client.base_url = "http://localhost:8080/produkt/";
+client.base_url = "http://localhost:8080/";
 client.get_uri = (function get_uri(uri,callback){
 return goog.net.XhrIo.send.call(null,uri,callback,"GET");
 });
 client.insert_data = (function insert_data(elem_id,e){
-var json__7126 = (e["target"]).getResponseJson();
-var data__7127 = cljs.core.js__GT_clj.call(null,json__7126,"﷐'keywordize-keys",true);
-var elem__7128 = goog.dom.getElement.call(null,elem_id);
-var d__7129 = goog.dom.htmlToDocumentFragment.call(null,cljs.core.str.call(null,"<span>","﷐'varenr".call(null,data__7127),"</span>&nbsp;<span>","﷐'navn".call(null,data__7127),"</span>"));
+var json__7347 = (e["target"]).getResponseJson();
+var data__7348 = cljs.core.js__GT_clj.call(null,json__7347,"﷐'keywordize-keys",true);
+var elem__7349 = goog.dom.getElement.call(null,elem_id);
+var d__7350 = goog.dom.htmlToDocumentFragment.call(null,cljs.core.str.call(null,"<span>","﷐'varenr".call(null,data__7348),"</span>&nbsp;<span>","﷐'navn".call(null,data__7348),"</span>"));
 
-goog.dom.removeChildren.call(null,elem__7128);
-return goog.dom.insertChildAt.call(null,elem__7128,d__7129,0);
+goog.dom.removeChildren.call(null,elem__7349);
+return goog.dom.insertChildAt.call(null,elem__7349,d__7350,0);
 });
 client.main = (function main(){
-return cljs.core.doall.call(null,cljs.core.map.call(null,(function (p1__7125_SHARP_){
-return client.get_uri.call(null,cljs.core.str.call(null,client.base_url,"120100",p1__7125_SHARP_),cljs.core.partial.call(null,client.insert_data,cljs.core.str.call(null,"navn",p1__7125_SHARP_)));
+alert.call(null,"MAIN");
+return cljs.core.doall.call(null,cljs.core.map.call(null,(function (p1__7346_SHARP_){
+return client.get_uri.call(null,cljs.core.str.call(null,client.base_url,"produkt/120100",p1__7346_SHARP_),cljs.core.partial.call(null,client.insert_data,cljs.core.str.call(null,"navn",p1__7346_SHARP_)));
 }),cljs.core.range.call(null,50)));
 });
 goog.exportSymbol('client.main', client.main);
+client.handle_file = (function handle_file(elem_id,e){
+var t__7351 = e.target.getResponseText();
+var elem__7352 = goog.dom.getElement.call(null,elem_id);
+
+return elem__7352.innerHTML = t__7351;
+});
+client.get_htmlfile_as_string = (function get_htmlfile_as_string(elem_id,file){
+alert.call(null,elem_id);
+return client.get_uri.call(null,cljs.core.str.call(null,client.base_url,file),cljs.core.partial.call(null,client.handle_file,elem_id));
+});
+goog.exportSymbol('client.get_htmlfile_as_string', client.get_htmlfile_as_string);
--- src/cs/client.cljs
+++ src/cs/client.cljs
@@ -2,7 +2,7 @@
   (:require [goog.net.XhrIo :as xhrio]
             [goog.dom :as dom]))
 
-(def base-url "http://localhost:8080/produkt/")
+(def base-url "http://localhost:8080/")
 
 (defn get-uri [uri callback]
   (goog.net.XhrIo/send uri callback "GET"))
@@ -17,4 +17,17 @@
     (dom/insertChildAt elem d 0)))
 
 (defn ^:export main []
-  (doall (map #(get-uri (str base-url "120100" %) (partial insert-data (str "navn" %))) (range 50))))
+  ((js* "alert") "MAIN")
+  (doall (map #(get-uri (str base-url "produkt/120100" %) (partial insert-data (str "navn" %))) (range 50))))
+
+(defn handle-file [elem-id e]
+  (let [t (. e/target (getResponseText))
+        elem (dom/getElement elem-id)]
+    ;;((js* "alert") t)
+    ;; (dom/insertChildAt elem t 0)
+    (set! (. elem innerHTML) t)
+    ))
+
+(defn ^:export get-htmlfile-as-string [elem-id file]
+  ((js* "alert") elem-id)
+  (get-uri (str base-url file) (partial handle-file elem-id)))
