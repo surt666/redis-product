ecd6363f2ca1f4f89c746d6895b830a1e98f5789
Steen Larsen <steen666@gmail.com>
2012-02-18 15:19:25 +0100

    simplification and internal wf

--- src/redis_product/mail.clj
+++ src/redis_product/mail.clj
@@ -5,35 +5,61 @@
   (:import [freemarker.template Configuration DefaultObjectWrapper]
            [java.io StringWriter File BufferedWriter FileWriter]))
 
+(def template-dir "/home/sla/templates")
+
 (defn configuration []
   (doto (Configuration.)
-    (.setDirectoryForTemplateLoading (File. "/home/sla/templates"))
+    (.setDirectoryForTemplateLoading (File. template-dir))
     (.setObjectWrapper (DefaultObjectWrapper.))))
 
 (def config (configuration))
 
+(declare ^:dynamic *message*)
+
+(declare ^:dynamic *data*)
+
+(declare ^:dynamic sendmail)
+
+(defn sendmail []
+  (let [res (send-message #^{:host "smtp.gmail.com"
+                             :user "steen666"
+                             :pass ""
+                             :ssl :yes!!!11}
+                          {:from (:from *data*)
+                           :to (:to *data*)
+                           :subject (:subject *data*)
+                           :body [{:type "text/html" :content *message*}]})]
+    (if (= 0 (:code res))
+      :ok
+      :bounce)))
+
+(defn sendsms []
+  (prn "send sms"))
+
+(defn sendspoc []
+  (prn "send to spoc"))
+
+(defmacro my-eval [s] `~(read-string s))
+
+(defn wf1 [] (condp = (sendmail)
+               :bounce (condp = (sendsms)
+                         :bounce (sendspoc))
+               :ok :ok))
+
 (defn merge-and-send [data]
-  (prn data)
-  (let [temp (. config (getTemplate (:template data)))
+  (let [temp (. config (getTemplate (str (:template data) ".ftl")))       
+        wf (slurp (str template-dir "/" (:template data) ".wf"))
         out (StringWriter.)
         _ (. temp (process (stringify-keys (:data data)) out))
-        mergedtemp (str out)]
-    (send-message #^{:host "smtp.gmail.com"
-                     :user "steen666"
-                     :pass ""
-                     :ssl :yes!!!11}
-                  {:from (:from data)
-                   :to (:to data)
-                   :subject (:subject data)
-                   :body [{:type "text/html" :content mergedtemp}]})))
+        mailbody (str out)]    
+    (binding [*message* mailbody
+              *data* data]      
+      (let [res (wf1)]
+        {:status (if (= :ok res) 204 404)}))))
 
 (defn save [data]
-  (let [html (BufferedWriter. (FileWriter. "/home/sla/templates/test2.ftl"))
-        wf (BufferedWriter. (FileWriter. "/home/sla/templates/test2.wf"))]
-    (. html (write (:msg data)))
-    (.close html)
-    (. wf (write (:workflow data)))
-    (.close wf)))
+  (spit "/home/sla/templates/test2.ftl" (str "[#ftl]\n" (:msg data)))
+  (spit "/home/sla/templates/test2.wf" (:workflow data)))
 
 (comment "{\"template\" : \"test.flt\",
   \"from\" : \"stel@yousee.dk\",
